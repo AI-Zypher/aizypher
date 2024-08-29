371fc7f94770c887de854d1fa072730c10295ac0
Krishna311204 <krishnappalekar31@gmail.com>
2024-08-29 11:38:28 +0530

    form update

--- src/app/components/forms.jsx
+++ src/app/components/forms.jsx
@@ -34,6 +34,7 @@ const RegistrationForm = () => {
             id="mobileNo"
             name="mobileNo"
             maxLength="10"
+            // pattern="[0-9]{10}"
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
             placeholder="Enter your mobile no."
           />
