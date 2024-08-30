1c8b4c55a2a87129121ec3a4e97a6458855e9b31
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-08-30 18:43:46 +0530

    added responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Second from "../components/second_comp";
 import Fourth from "../components/mirroimage";
 
 export default function Home() {
-  return (
+return (
     <div className="App">
       <First />
       <Second />
