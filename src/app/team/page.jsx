d5e8d0ddaa0828069c786f53927b4188225e5f7b
Uppili <uppilibalaji2005@gmail.com>
2024-09-11 20:12:22 +0530

    minor changes

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -30,7 +30,7 @@ export default function TeamPage() {
       <div className="relative mx-auto w-full max-w-sm mb-[5vh]" key={section.id}>
         <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
           <Image
-            src={`jordans.webp`}
+            src={`/jordans.webp`}
             alt="jordans"
             height="400"
             width="400"
