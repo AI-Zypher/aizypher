b853c7f06113beca448362351b85b9138af22ceb
Uppili <uppilibalaji2005@gmail.com>
2024-09-18 21:21:54 +0530

    college name build

--- src/app/[eventdesc]/[register]/page.jsx
+++ src/app/[eventdesc]/[register]/page.jsx
@@ -153,8 +153,8 @@ export default function RegistrationForm({ params }) {
             </label>
             <input
               type="text"
-              id="name"
-              name="name"
+              id="College name"
+              name="College name"
               value={formData.College_name}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
